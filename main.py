afb83ed84bf50ca82c3e1da5ae6276c369d57d10
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-12-02 17:50:38 +0000

    Working on db connection (sqllite3)

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import models
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
+
+class StockRequest(BaseModel):
+    symbol: str
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home(request: Request):
@@ -21,7 +32,7 @@ def home(request: Request):
         })
 
 @app.post("/stock")
-def create_stock():
+def create_stock(stock_request: StockRequest):
     """
     creates ticker and stores it in the database
     """
